4fe23ce1d1cf19746aa15c1b111401913e8cc6e7
kevinjin420 <kevinjin0420@gmail.com>
2026-03-08 01:16:12 -0500

    build verbosity

--- build.sh
+++ build.sh
@@ -3,5 +3,11 @@
 set -euxo pipefail
 
 PRESET="${1:-dev}"
+
+echo "==> Configuring with preset: $PRESET"
 cmake --preset "$PRESET"
+
+echo "==> Building with preset: $PRESET"
 cmake --build --preset "$PRESET" -j2
+
+echo "==> Build complete"
