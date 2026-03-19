953f22df3da22ee760df10ed69cb0e3881baf1d5
kevinjin420 <kevinjin0420@gmail.com>
2026-03-19 03:00:42 -0400

    upside down texture

--- src/display/display.cpp
+++ src/display/display.cpp
@@ -6,16 +6,16 @@
 #include "resource_dir.h"
 #include "state_machine/state_machine.h"
 
+static RenderTexture2D framebuffer;
+
 void InitDisplay() {
 	SearchAndSetResourceDir("resources");
+	framebuffer = LoadRenderTexture(screenRes, screenRes);
 	InitSky();
 	InitHud();
 }
 
-void DrawFrame() {
-	BeginDrawing();
-	ClearBackground(BLACK);
-
+static void DrawScene() {
 	switch (GetTelescopeState()) {
 	case TelescopeState::INIT:
 		DrawHud();
@@ -41,10 +41,26 @@ void DrawFrame() {
 		DrawText("Found", screenRes / 2, screenRes / 2, 40, WHITE);
 		break;
 	}
+}
 
+void DrawFrame() {
+	BeginTextureMode(framebuffer);
+	ClearBackground(BLACK);
+	DrawScene();
+	EndTextureMode();
+
+	BeginDrawing();
+	ClearBackground(BLACK);
+	// Flip vertically: positive height in src = mirrored across x-axis
+	// (RenderTextures are already y-flipped in OpenGL, so using positive
+	// height flips it an extra time, giving the mirror effect)
+	Rectangle src = {0, 0, (float)screenRes, (float)screenRes};
+	Rectangle dst = {0, 0, (float)screenRes, (float)screenRes};
+	DrawTexturePro(framebuffer.texture, src, dst, {0, 0}, 0, WHITE);
 	EndDrawing();
 }
 
 void CleanupDisplay() {
 	CleanupSky();
+	UnloadRenderTexture(framebuffer);
 }
